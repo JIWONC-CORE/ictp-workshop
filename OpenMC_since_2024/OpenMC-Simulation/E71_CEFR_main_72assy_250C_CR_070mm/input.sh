6789b600b8b07ed01e20070a6df3f2614b0f038a
JiwonChoe <chi91023@unist.ac.kr>
2025-09-24 19:05:13 +0900

    OpenMC input files
    OpenMC input files are updated to be able to run with the latest OpenMC.

---
+++ OpenMC_since_2024/OpenMC-Simulation/E71_CEFR_main_72assy_250C_CR_070mm/input.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+dir="${PWD##*/}"
+
+rm $dir.py
+rm *.py *.ascii *.log *.h5 *.xml
+
+echo "import openmc" > $dir.py
+
+
+# Main input file
+# -------------------------------------------------------
+# --- Include files
+# -------------------------------------------------------
+# 
+# - Surfaces
+cat ../../link_files/CEFR-01-surf-250C.omc         >> $dir.py # All surface specs
+# - Materials
+cat ../../link_files/CEFR-02-mat-1-all-250C.omc    >> $dir.py # Fuel and Nnon-fuel material specs
+# - Pins
+cat ../../link_files/CEFR-03-pin-1-FU.omc          >> $dir.py # Definition of fuel pins
+cat ../../link_files/CEFR-03-pin-2-CR.omc          >> $dir.py # Definition of CR pins
+cat ../../link_files/CEFR-03-pin-3-RR.omc          >> $dir.py # Definition of radial reflector and boron shield pins
+# # - Assembly level lattices
+cat ../../link_files/CEFR-04-lat-1-FU.omc          >> $dir.py # Definition of fuel lattices
+cat ../../link_files/CEFR-04-lat-2-CR.omc          >> $dir.py # Definition of CR lattices
+cat ../../link_files/CEFR-04-lat-3-RR.omc          >> $dir.py # Definition of radial reflector lattices
+cat ../../link_files/CEFR-04-lat-4-RB.omc          >> $dir.py # Definition of radial shield lattices
+cat ../../link_files/CEFR-04-lat-5-NS.omc          >> $dir.py # Definition of neutron source lattices
+# # - Separate assemblies                        
+cat ../../link_files/CEFR-05-asy-1-FU.omc          >> $dir.py # Definition of fuel assemblies
+#cat ../../link_files/CEFR-05-asy-2-CR.omc          >> $dir.py # Definition of CR with enriched B4C
+cat ../../link_files/CEFR-05-asy-3-RR.omc          >> $dir.py # Definition of radial reflector assemblies
+cat ../../link_files/CEFR-05-asy-4-RB.omc          >> $dir.py # Definition of radial shield assemblies
+cat ../../link_files/CEFR-05-asy-5-NS.omc          >> $dir.py # Definition of neutron source assembly
+cat ../../link_files/CEFR-05-asy-6-MU.omc          >> $dir.py # Definition of mock-up assembly
+# # - Control Rods Position 
+cat ../../link_files/CEFR-07-rod-position-4-RE2-70mm.omc             >> $dir.py # Control rod positions
+
+# # - Core
+cat ../../link_files/CEFR-06-core-lat-72assy-250C.omc  >> $dir.py # Core lattice specs
+
+echo "# Instantiate a Geometry, register the root Universe, and export to XML" >> $dir.py 
+echo "geometry = openmc.Geometry(root)                                       " >> $dir.py 
+echo "geometry.export_to_xml()                                               " >> $dir.py 
+
+# -------------------------------------------------------
+# --- Run options
+# -------------------------------------------------------
+cat ../../link_files/SET-01-BASIC.omc >> $dir.py
+cat ../../link_files/SET-02-PLOT.omc  >> $dir.py
+cat ../../link_files/SET-03-TALLY.omc >> $dir.py
+
+# -------------------------------------------------------
+# --- Simulate OpenMC
+# -------------------------------------------------------
+#python3.10 ./$dir.py
+#openmc -p 
+##nohup openmc -s $1 > $dir.log && h5dump summary.h5 > summary.ascii &
+#openmc -s $1 > $dir.log && h5dump summary.h5 > summary.ascii 
+
